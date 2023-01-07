2252c5700bca05ea000afbe0977a67b5d1799a08
Eduardo Blancas <github@blancas.io>
2023-01-07 12:51:51 -0600

    debuglater release 1.4.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CHANGELOG
 
-## 1.4.4dev
+## 1.4.4 (2023-01-07)
 * `save_dump` creates all parent directories if they don't exist
 
 ## 1.4.3 (2022-08-22)
--- src/debuglater/__init__.py
+++ src/debuglater/__init__.py
@@ -3,4 +3,4 @@ from debuglater.ipython import patch_ipython
 
 __all__ = ["run", "excepthook_factory", "patch_ipython", "debug_dump"]
 
-__version__ = "1.4.4dev"
+__version__ = "1.4.4"
